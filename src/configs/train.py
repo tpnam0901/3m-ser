0f5f540fd4e327a8ac0c023a1554dc143d196e94
namphuongtran9196 <namphuongtran9196@gmail.com>
2023-09-06 08:23:14 +0700

    chore: auto set name config

--- src/configs/train.py
+++ src/configs/train.py
@@ -5,7 +5,6 @@ class Config(BaseConfig):
     # Base
     def __init__(self, **kwargs):
         super(Config, self).__init__(**kwargs)
-        self.name = "3m-ser_roberta_wav2vec2"
         self.add_args()
         for key, value in kwargs.items():
             setattr(self, key, value)
@@ -32,17 +31,20 @@ class Config(BaseConfig):
         self.focal_loss_gamma = 0.5
         self.focal_loss_alpha = None
 
-        self.model_type = "MMSERA_v2"
-        self.text_encoder_type = "roberta"  # [bert, roberta]
+        self.model_type = "MMSERA_v2"  # [MMSERA, AudioOnly, TextOnly, SERVER, *_v2]
+        self.text_encoder_type = "bert"  # [bert, roberta]
         self.text_encoder_dim = 768
         self.text_unfreeze = False
-        self.audio_encoder_type = "wav2vec2_base"  # [vggish, panns, hubert_base, wav2vec2_base, wavlm_base]
-        self.audio_encoder_dim = 768  # 2048 - panns, 128 - vggish, 768 - hubert_base,wav2vec2_base, wavlm_base
+        self.audio_encoder_type = "vggish"  # [vggish, panns, hubert_base, wav2vec2_base, wavlm_base]
+        self.audio_encoder_dim = 128  # 2048 - panns, 128 - vggish, 768 - hubert_base,wav2vec2_base, wavlm_base
         self.audio_norm_type = "layer_norm"  # [layer_norm, min_max, None]
         self.audio_unfreeze = False
 
         self.fusion_head_output_type = "cls"  # [cls, mean, max]
         self.linear_layer_output = [64]
+
+        # Config name
+        self.name = f"3m-ser_{self.text_encoder_type}_{self.audio_encoder_type}_{self.fusion_head_output_type}"
 
         for key, value in kwargs.items():
             setattr(self, key, value)
