5811b81d18b8357564353023d8dcee8b2329f7e3
namphuongtran9196 <namphuongtran9196@gmail.com>
2024-02-20 11:07:41 +0700

    feat: add confusion matrix chore: remove duplicate metrics

--- scripts/eval.py
+++ scripts/eval.py
@@ -11,29 +11,28 @@ logging.basicConfig(
 import csv
 import argparse
 import torch
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
 from sklearn import svm
-from sklearn.metrics import (
-    balanced_accuracy_score,
-    accuracy_score,
-)
+from sklearn.metrics import balanced_accuracy_score, accuracy_score, confusion_matrix
 from data.dataloader import build_train_test_dataset
 from tqdm.auto import tqdm
 from models import networks
 from configs.base import Config
-
 from collections import Counter
 
 
-def calculate_accuracy(y_pred, y_true):
+def calculate_accuracy(y_true, y_pred):
     class_weights = {cls: 1.0 / count for cls, count in Counter(y_true).items()}
-    wa = balanced_accuracy_score(
+    bacc = balanced_accuracy_score(
         y_true, y_pred, sample_weight=[class_weights[cls] for cls in y_true]
     )
-    ua = accuracy_score(y_true, y_pred)
-    return ua, wa
+    acc = accuracy_score(y_true, y_pred)
+    return bacc, acc
 
 
-def eval(cfg, checkpoint_path, all_state_dict=True):
+def eval(cfg, checkpoint_path, all_state_dict=True, cm=False):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     network = getattr(networks, cfg.model_type)(cfg)
     network.to(device)
@@ -63,23 +62,37 @@ def eval(cfg, checkpoint_path, all_state_dict=True):
             _, preds = torch.max(output, 1)
             y_actu.append(label.detach().cpu().numpy()[0])
             y_pred.append(preds.detach().cpu().numpy()[0])
-    bacc = balanced_accuracy_score(y_actu, y_pred)
-    ua, wa = calculate_accuracy(y_actu, y_pred)
-    return bacc, ua, wa
+    bacc, acc = calculate_accuracy(y_actu, y_pred)
+    if cm:
+        cm = confusion_matrix(y_actu, y_pred)
+        print("Confusion Matrix: \n", cm)
+        cmn = (cm.astype("float") / cm.sum(axis=1)[:, np.newaxis]) * 100
 
+        ax = plt.subplots(figsize=(8, 5.5))[1]
+        sns.heatmap(
+            cmn,
+            cmap="YlOrBr",
+            annot=True,
+            square=True,
+            linecolor="black",
+            linewidths=0.75,
+            ax=ax,
+            fmt=".2f",
+            annot_kws={"size": 16},
+        )
+        ax.set_xlabel("Predicted", fontsize=18, fontweight="bold")
+        ax.xaxis.set_label_position("bottom")
+        ax.xaxis.set_ticklabels(
+            ["Anger", "Happiness", "Sadness", "Neutral"], fontsize=16
+        )
+        ax.set_ylabel("Ground Truth", fontsize=18, fontweight="bold")
+        ax.yaxis.set_ticklabels(
+            ["Anger", "Happiness", "Sadness", "Neutral"], fontsize=16
+        )
+        plt.tight_layout()
+        plt.savefig(cfg.name + ".png", format="png", dpi=1200)
 
-def arg_parser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "-ckpt", "--checkpoint_path", type=str, help="path to checkpoint folder"
-    )
-    parser.add_argument(
-        "-r",
-        "--recursive",
-        action="store_true",
-        help="whether to travel child folder or not",
-    )
-    return parser.parse_args()
+    return bacc, acc
 
 
 def find_checkpoint_folder(path):
@@ -92,13 +105,14 @@ def find_checkpoint_folder(path):
     return list_candidates
 
 
-def main(root_path):
+def main(root_path, cm):
     logging.info("Finding checkpoints")
     list_checkpoints = find_checkpoint_folder(root_path)
-    csv_path = os.path.basename(root_path) + ".csv"
+    test_set = args.test_set if args.test_set is not None else "test.pkl"
+    csv_path = os.path.basename(root_path) + "{}.csv".format(test_set)
     # field names
-    fields = ["Model", "Settings", "Time", "BACC", "UA", "WA"]
-    with open(csv_path, "w") as csvfile:
+    fields = ["BACC", "ACC", "Time", "Model", "Settings"]
+    with open(csv_path, "a") as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=fields)
         writer.writeheader()
         for ckpt in list_checkpoints:
@@ -112,21 +126,50 @@ def main(root_path):
             cfg = Config()
             cfg.load(cfg_path)
             # Change to test set
-            cfg.data_valid = "test.pkl"
-            bacc, ua, wa = eval(cfg, ckpt_path)
+            cfg.data_valid = test_set
+            bacc, acc = eval(cfg, ckpt_path, cm=cm)
             writer.writerows(
                 [
                     {
+                        "BACC": round(bacc * 100, 2),
+                        "ACC": round(acc * 100, 2),
+                        "Time": time,
                         "Model": model_name,
                         "Settings": settings,
-                        "Time": time,
-                        "BACC": round(bacc, 3),
-                        "UA": round(ua, 3),
-                        "WA": round(wa, 3),
                     }
                 ]
             )
-            logging.info("BACC {:.3f} | UA {:.3f} | WA {:.3f}".format(bacc, ua, wa))
+            logging.info("BACC {:.3f} | ACC {:.3f}".format(bacc, acc))
+
+
+def arg_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-ckpt", "--checkpoint_path", type=str, help="path to checkpoint folder"
+    )
+    parser.add_argument(
+        "-r",
+        "--recursive",
+        action="store_true",
+        help="whether to travel child folder or not",
+    )
+
+    parser.add_argument(
+        "-t",
+        "--test_set",
+        type=str,
+        default=None,
+        help="name of testing set. Ex: test.pkl",
+    )
+
+    parser.add_argument(
+        "-cm",
+        "--confusion_matrix",
+        action="store_true",
+        help="whether to export consution matrix or not",
+    )
+
+    return parser.parse_args()
 
 
 if __name__ == "__main__":
@@ -140,7 +183,7 @@ if __name__ == "__main__":
         cfg.load(cfg_path)
         # Change to test set
         cfg.data_valid = "test.pkl"
-        bacc, ua, wa = eval(cfg, ckpt_path)
-        logging.info("BACC {:.3f} | UA {:.3f} | WA {:.3f}".format(bacc, ua, wa))
+        bacc, acc = eval(cfg, ckpt_path, cm=args.confusion_matrix)
+        logging.info("BACC {:.3f} | ACC {:.3f}".format(bacc, acc))
     else:
-        main(args.checkpoint_path)
+        main(args.checkpoint_path, args.confusion_matrix)
